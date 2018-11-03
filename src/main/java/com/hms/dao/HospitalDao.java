f36623fc5228e1ec9ee7fa581cfb575a5753543d
Dilan-Madusanka <dilanmax93@gmail.com>
2018-11-03 05:36:55 +0530

    Initial Commit

---
+++ src/main/java/com/hms/dao/HospitalDao.java
@@ -0,0 +1,9 @@
+package com.hms.dao;
+
+import java.util.List;
+
+import com.hms.model.Hospital;
+
+public interface HospitalDAO {
+	public List<Hospital> getHospital();
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+insert into Hospital(address,contact_number,name,website)
+values('Colombo',0112585758,'City Care Hospital','info@citycarehospital.com');
+
+insert into Doctor(address,contact_number,degree,designation,email_id,experience,name) 
+values('Colombo',0777564525,'MBBS','Pediatrician','samaira@hospital.com', 6,'Samaira');
+insert into Doctor(address,contact_number,degree,designation,email_id,experience,name) 
+values('Ragama', 0777565235,'MD','Cardiologist','john@hospital.com', 12, 'John');
+insert into Doctor(address,contact_number,degree,designation,email_id,experience,name) 
+values('Galle',0774844525, 'MBBS', 'Gynecologist','peter@hospital.com', 3, 'Peter');
+insert into Doctor(address,contact_number,degree,designation,email_id,experience,name) 
+values('Colombo',0702525525, 'MBBS', 'Neurologist', 'shoun@hospital.com',10, 'Shuon');
+insert into Doctor(address,contact_number,degree,designation,email_id,experience,name) 
+values('Matara', 0712520525,'MS','Surgeon', 'saman@hospital.com',7, 'Saman');
+
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Rathmalana',0712221685,'Fever',1,'nuwan@mail.com','Male','Nuwan');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Kadawatha',0702548515,'Cold',1,'neha@mail.com','Female','Neha');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Nugegoda',0712522145,'Head Ache',4,'nilesh@mail.com','Male','Nilesh');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Kiribathgoda',0712210035,'Malaria',3,'kavita@mail.com','Female','Kavita');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Kadawatha',0774479525,'Cold',1,'ramesh@mail.com','Male','Ramesh');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Ampara',0777335415,'Heart Problem',2,'asela@mail.com','Male','Asela');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Galle',0702561085,'Dengue',3,'kasun@mail.com','Male','Kasun');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Kottawa',0777564488,'Kidney Stone',5,'wimala@mail.com','Female','Wimala');
+insert into Patient(address,contact_number,disease,doctor_doctor_id,email_id,gender,name)
+values('Gampaha',0775694585,'Cough', 1,'lahiru@mail.com','Male','Lahiru');
